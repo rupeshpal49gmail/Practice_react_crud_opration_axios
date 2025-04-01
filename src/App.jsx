ac2a8c0ed03057ece02a5280c2c4e99e550a31a1
Rupesh Pal <rupeshpal49@gmail.com>
2025-04-02 01:08:30 +0530

    add css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useEffect } from "react";
-import { getPost } from "./Api/PostApi";
 import Posts from "./components/Posts";
-// import "./App.css";
+import "./App.css";
 
 function App() {
   return (
-    <>
+    <section className="main-section">
       <Posts />
-    </>
+    </section>
   );
 }
 
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,19 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { getPost } from "../Api/PostApi";
+import "../App.css";
 
 const Posts = () => {
+  const [data, setData] = useState([]);
   const getPostData = async () => {
     const res = await getPost();
     console.log(res.data);
+    setData(res.data);
   };
 
   useEffect(() => {
     getPostData();
   }, []);
   return (
-    <>
-      <h1>Hello Post methods</h1>
-    </>
+    <section className="section-post">
+      <ul>
+        {data.map((curElement) => {
+          const { id, body, title } = curElement;
+          return (
+            <li key={id}>
+              <p>Title: {title}</p>
+              <p>Body: {body}</p>
+              <button>Edit</button>
+              <button className="btn-delete">Delete</button>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
   );
 };
 
